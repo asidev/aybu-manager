6115315136c324bd084570f0ffb7b176a0bb554a
Giacomo Bagnoli <g.bagnoli@asidev.com>
2012-02-01 12:50:00 +0100

    Empty response.body when setting content length header to 0. Fixes bug when running behind uWSGI

--- aybu/manager/rest/views/exceptions.py
+++ aybu/manager/rest/views/exceptions.py
@@ -51,6 +51,7 @@ def generate_empty_response(context, request, status, add_headers={}):
         {'Content-Length': 0,
          'Content-Type': 'application/json; charset=UTF-8'}
     )
+    response.body = ''
     response.headers.update(add_headers)
     # TODO add logging with ip/user/agent etc etc
     log.info("Generating empty response [status: %s, headers:%s]",
